4ae9e2377b3979077df32607bea06f8e45e7f808
thuongb14 <thuongb14@gmail.com>
2022-11-08 21:51:09 +1100

    add .

--- models/database.py
+++ models/database.py
@@ -1,15 +1,19 @@
 import psycopg2
 
+import os
+
+DB_URL = os.environ.get('DATABASE_URL', 'dbname=quotes')
+
 #Reading from the database
 def sql_select(query, params = []):
-    conn = psycopg2.connect('dbname=quotes')
+    conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
     cur.execute(query, params)
     return cur.fetchall()
 
 #Writing to the database    
 def sql_write(query, params = []):
-    conn = psycopg2.connect('dbname=quotes')
+    conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
     cur.execute(query, params)
     conn.commit()
